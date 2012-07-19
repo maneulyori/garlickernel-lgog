afdf1ecc48bd8f82f7753c90d62ee91d43f89e73
Tejun Heo <tj@kernel.org>
2012-07-19 13:52:53 -0700

    kthread_worker: reorganize to prepare for flush_kthread_work() reimplementation
    Make the following two non-functional changes.
    
    * Separate out insert_kthread_work() from queue_kthread_work().
    
    * Relocate struct kthread_flush_work and kthread_flush_work_fn()
      definitions above flush_kthread_work().
    
    v2: Added lockdep_assert_held() in insert_kthread_work() as suggested
        by Andy Walls.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Andy Walls <awalls@md.metrocast.net>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    
    	kernel/kthread.c

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -382,7 +382,11 @@ static void insert_kthread_work(struct kthread_worker *worker,
 	lockdep_assert_held(&worker->lock);
 
 	list_add_tail(&work->node, pos);
+<<<<<<< HEAD
 	work->worker = worker;
+=======
+	work->queue_seq++;
+>>>>>>> 8341ae5... kthread_worker: reorganize to prepare for flush_kthread_work() reimplementation
 	if (likely(worker->task))
 		wake_up_process(worker->task);
 }
@@ -457,6 +461,7 @@ retry:
 	else
 		noop = true;
 
+<<<<<<< HEAD
 	spin_unlock_irq(&worker->lock);
 
 	if (!noop)
@@ -464,6 +469,8 @@ retry:
 }
 EXPORT_SYMBOL_GPL(flush_kthread_work);
 
+=======
+>>>>>>> 8341ae5... kthread_worker: reorganize to prepare for flush_kthread_work() reimplementation
 /**
  * flush_kthread_worker - flush all current works on a kthread_worker
  * @worker: worker to flush
